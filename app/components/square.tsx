7b387dfdb3361c83476d7507151121c5a0964a17
GabrielWarner <98490756+GabrielWarner@users.noreply.github.com>
2023-11-15 20:44:55 -0800

    square and board component setup

---
+++ app/components/square.tsx
@@ -0,0 +1,4 @@
+import styles from '../styles.module.css'
+export default function Square({ value }) {
+    return <button className={styles.square}>{value}</button>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import Board from './components/board.tsx'
+// import styles from './page.module.css'
+import Square from './components/square'
+import styles from './styles.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <nav>
+        <Board/>
       </nav>
     </main>
   )
