a1ba0e21e1728232709a2dd15245d76f1ee3b4b5
GabrielWarner <98490756+GabrielWarner@users.noreply.github.com>
2023-11-16 22:52:55 -0800

    added use state to square, added types to use state

--- app/components/square.tsx
+++ app/components/square.tsx
@@ -1,4 +1,14 @@
+'use client'
+import { useState } from 'react'
+
 import styles from '../styles.module.css'
-export default function Square({ value }) {
-    return <button className={styles.square}>{value}</button>
+export default function Square({ }) {
+    const [value, setValue] = useState<string | null>(null)
+
+    function handleClick() {
+        setValue('X')
+    }
+    return(
+        <button className={styles.square} onClick={handleClick}>{value}</button>
+    )
 }
